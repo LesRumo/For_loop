977454558428de260de4a5421f1fa7d71b8486ba
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-06 10:59:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("while loop fo reference");
+            int i = 0;
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("The for loop");
+            for(int j=0;j<=5;j++)
+            {
+                Console.WriteLine(j);
+            }
+            Console.ReadLine();
+        }
+    }
+}
